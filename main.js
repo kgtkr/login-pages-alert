90627fc5037836a2266dc997691a452bf8b68d65
kgtkr <kgtkr.jp@gmail.com>
2019-02-27 21:18:34 +0900

    だめ

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 (() => {
   function insertHTML(type) {
+    close();
     const el = document.createElement("div");
     el.className = "login-pages-alert";
     const msg = document.createElement("div");
@@ -13,14 +14,34 @@
     const button = document.createElement("a");
     button.className = "close";
     button.innerText = "[閉じる]";
-    button.onclick = () => {
-      for (let el of document.getElementsByClassName("login-pages-alert")) {
-        document.body.removeChild(el);
-      }
-    };
+    button.onclick = close;
     el.appendChild(button);
 
     document.body.insertAdjacentElement("afterbegin", el);
+  }
+
+  function close() {
+    for (let el of document.getElementsByClassName("login-pages-alert")) {
+      document.body.removeChild(el);
+    }
+  }
+
+  function pageLoad() {
+    setTimeout(() => {
+      close();
+      const body = document.body.innerHTML;
+
+      loop: for (let site of list) {
+        if (location.href.includes(site.url)) {
+          for (let data of site.data) {
+            if (data.matchs.every(x => body.includes(x))) {
+              insertHTML(data.type);
+              break loop;
+            }
+          }
+        }
+      }
+    }, 1000);
   }
 
   const list = [
@@ -30,23 +51,54 @@
         {
           type: "有料",
           matchs: ["この先は有料会員の登録が必要です。"]
+        },
+      ]
+    },
+    {
+      url: "https://www.nikkei.com",
+      data: [
+        {
+          type: "",
+          matchs: ["この記事は会員限定です。"]
+        }
+      ]
+    },
+    {
+      url: "https://note.mu",
+      data: [
+        {
+          type: "有料",
+          matchs: ["この続きをみるには"]
         }
       ]
     }
   ];
 
   window.addEventListener("load", () => {
-    const body = document.body.innerHTML;
-
-    loop: for (let site of list) {
-      if (location.href.includes(site.url)) {
-        for (let data of site.data) {
-          if (data.matchs.some(x => body.includes(x))) {
-            insertHTML(data.type);
-            break loop;
-          }
-        }
-      }
-    }
+    pageLoad();
   }, false);
+
+  function addCallback(cons, name, f) {
+    cons.prototype[name] = function (...params) {
+      console.log(cons, name);
+      const res = this[name](...params);
+      f(params, res);
+      return res;
+    };
+  }
+  addCallback(History, "pushState", () => {
+    pageLoad();
+  });
+  addCallback(History, "replaceState", () => {
+    pageLoad();
+  });
+  addCallback(History, "back", () => {
+    pageLoad();
+  });
+  addCallback(History, "forward", () => {
+    pageLoad();
+  });
+  addCallback(History, "go", () => {
+    pageLoad();
+  });
 })();
