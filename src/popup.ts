1d250502dc66e954f590fbfb531e62abe8f07cda
kgtkr <kgtkr.jp@gmail.com>
2019-02-28 22:04:04 +0900

    Firefox対応

--- src/popup.ts
+++ src/popup.ts
@@ -1,7 +1,9 @@
-Array.from(document.getElementsByTagName("a"))
-  .forEach(x => {
-    const href = x.href;
-    x.addEventListener("click", () => {
-      chrome.tabs.create({ active: true, url: href });
+if (navigator.userAgent.includes("Chrome")) {
+  Array.from(document.getElementsByTagName("a"))
+    .forEach(x => {
+      const href = x.href;
+      x.addEventListener("click", () => {
+        chrome.tabs.create({ active: true, url: href });
+      });
     });
-  });
+}
