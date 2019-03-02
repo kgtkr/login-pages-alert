ddbad5c0461973eac29c4a6f4426cd74a9410edc
kgtkr <kgtkr.jp@gmail.com>
2019-03-02 21:53:03 +0900

    UrlPattern

---
+++ src/options/src/UrlPattern.elm
@@ -0,0 +1,32 @@
+module UrlPattern exposing (Model, Msg(..), update, view)
+
+import Html exposing (Html, button, div, input, text)
+import Html.Attributes exposing (placeholder, value)
+import Html.Events exposing (onClick, onInput)
+import List.Extra as LE
+import Type
+
+
+type alias Model =
+    Type.UrlPattern
+
+
+type Msg
+    = ChangeOrigin String
+    | ChangePathname String
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ input [ value model.origin, onInput ChangeOrigin, placeholder "origin" ] [], input [ value model.pathname, onInput ChangePathname, placeholder "pathname" ] [] ]
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        ChangeOrigin s ->
+            { model | origin = s }
+
+        ChangePathname s ->
+            { model | pathname = s }
