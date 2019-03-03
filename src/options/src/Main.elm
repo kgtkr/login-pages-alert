f4be3ef417996997e719722aa674ea19d4cfb1f6
kgtkr <kgtkr.jp@gmail.com>
2019-03-03 12:57:24 +0900

    Main

--- src/options/src/Main.elm
+++ src/options/src/Main.elm
@@ -1,7 +1,12 @@
 module Main exposing (Flag, Model, Msg(..), init, main, subscriptions, update, view)
 
 import Browser
-import Html exposing (Html, div)
+import Html exposing (Html, button, div, input, text)
+import Html.Attributes exposing (placeholder, value)
+import Html.Events exposing (onClick, onInput)
+import List.Extra as LE
+import Site
+import Type
 
 
 main =
@@ -18,26 +23,38 @@ type alias Flag =
 
 
 type alias Model =
-    {}
+    List Type.Site
 
 
 type Msg
-    = None
+    = ChangeSite Int Site.Msg
+    | RemoveSite Int
+    | AddSite
 
 
 init : Flag -> ( Model, Cmd Msg )
 init flag =
-    ( {}, Cmd.none )
+    ( [], Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
-    div [] []
+    div [] (div [] [ button [ onClick AddSite ] [ text "+" ] ] :: (model |> List.indexedMap (\i x -> [ button [ onClick (RemoveSite i) ] [ text "☓" ], x |> Site.view |> Html.map (ChangeSite i) ]) |> List.concat))
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    ( case msg of
+        AddSite ->
+            { urlPatterns = [], kinds = [] } :: model
+
+        RemoveSite i ->
+            LE.removeAt i model
+
+        ChangeSite i m ->
+            LE.updateAt i (Site.update m) model
+    , Cmd.none
+    )
 
 
 subscriptions : Model -> Sub Msg
