6c38450c3597c107d3ce2c8d889367e8edf8dde8
kgtkr <kgtkr.jp@gmail.com>
2019-03-03 14:55:28 +0900

    効率化

--- src/app.ts
+++ src/app.ts
@@ -1,82 +1,89 @@
 import * as matcher from "matcher";
 import { loadData } from "./common";
 
-const list = loadData();
+const list = loadData().filter(site => site.urlPatterns.some(x => matcher.isMatch(location.origin, x.origin, { caseSensitive: true })));
+function main() {
+  let cur = location.href;
+  //null:開いてない、undefined:閉じた、文字列:type
+  let isOpen: string | null | undefined = null;
 
-let cur = location.href;
-//null:開いてない、undefined:閉じた、文字列:type
-let isOpen: string | null | undefined = null;
-
-function insertHTML(kind: string) {
-  if (isOpen === undefined) {
-    return;
-  }
-
-  if (isOpen !== kind) {
-    close();
-    isOpen = null;
-  }
-
-  if (isOpen === null) {
-    const el = document.createElement("div");
-    el.className = "login-pages-alert";
-    const msg = document.createElement("div");
-    msg.className = "msg";
-    msg.innerText = `このページは最後まで読むのに${kind}会員登録が必要です。`;
-    el.appendChild(msg);
-    const by = document.createElement("div");
-    by.className = "by";
-    by.innerText = "by Login Pages Alert";
-    el.appendChild(by);
-    const button = document.createElement("a");
-    button.className = "close";
-    button.innerText = "[閉じる]";
-    button.onclick = close;
-    el.appendChild(button);
-
-    document.body.insertAdjacentElement("afterbegin", el);
-    isOpen = kind;
-  }
-}
-
-function close() {
-  if (typeof isOpen === "string") {
-    for (let el of Array.from(document.getElementsByClassName("login-pages-alert"))) {
-      document.body.removeChild(el);
+  function insertHTML(kind: string) {
+    if (isOpen === undefined) {
+      return;
     }
-    isOpen = undefined;
-  }
-}
 
-function run() {
-  console.log("login-pages-alert:run");
-  if (cur !== location.href) {
-    close();
-    cur = location.href;
-    isOpen = null;
-  }
-  const body = document.body.innerHTML;
+    if (isOpen !== kind) {
+      close();
+      isOpen = null;
+    }
 
-  for (let site of list) {
-    if (site.urlPatterns.some(url => matcher.isMatch(location.origin, url.origin, { caseSensitive: true }) && matcher.isMatch(location.pathname, url.pathname, { caseSensitive: true }))) {
-      for (let kind of site.kinds) {
-        if (kind.bodyPatterns.some(x => x.every(x => body.includes(x)))) {
-          insertHTML(kind.name);
-          return;
+    if (isOpen === null) {
+      const el = document.createElement("div");
+      el.className = "login-pages-alert";
+      const msg = document.createElement("div");
+      msg.className = "msg";
+      msg.innerText = `このページは最後まで読むのに${kind}会員登録が必要です。`;
+      el.appendChild(msg);
+      const by = document.createElement("div");
+      by.className = "by";
+      by.innerText = "by Login Pages Alert";
+      el.appendChild(by);
+      const button = document.createElement("a");
+      button.className = "close";
+      button.innerText = "[閉じる]";
+      button.onclick = close;
+      el.appendChild(button);
+
+      document.body.insertAdjacentElement("afterbegin", el);
+      isOpen = kind;
+    }
+  }
+
+  function close() {
+    if (typeof isOpen === "string") {
+      for (let el of Array.from(document.getElementsByClassName("login-pages-alert"))) {
+        document.body.removeChild(el);
+      }
+      isOpen = undefined;
+    }
+  }
+
+  function run() {
+    console.log("login-pages-alert:run");
+    if (cur !== location.href) {
+      close();
+      cur = location.href;
+      isOpen = null;
+    }
+    const body = document.body.innerHTML;
+
+    for (let site of list) {
+      if (site.urlPatterns.some(url => matcher.isMatch(location.pathname, url.pathname, { caseSensitive: true }))) {
+        for (let kind of site.kinds) {
+          if (kind.bodyPatterns.some(x => x.every(x => body.includes(x)))) {
+            insertHTML(kind.name);
+            return;
+          }
         }
       }
     }
+
+    close();
   }
 
-  close();
+  window.addEventListener("load", () => {
+    run();
+  }, false);
+
+  new MutationObserver(mutations => {
+    if (mutations.length !== 0) {
+      run();
+    }
+  }).observe(document.body, { childList: true });
+
 }
 
-window.addEventListener("load", () => {
-  run();
-}, false);
+if (list.length !== 0) {
+  main();
+}
 
-new MutationObserver(mutations => {
-  if (mutations.length !== 0) {
-    run();
-  }
-}).observe(document.body, { childList: true });
